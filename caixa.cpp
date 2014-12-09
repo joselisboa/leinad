c6050c019ee1fa47a60e1048d9b0bfc3a8d2c315
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-12-09 19:17:47 +0000

    Add Caixa

---
+++ caixa.cpp
@@ -0,0 +1,51 @@
+#include "caixa.h"
+
+COORD const MAX = { 120, 60 };// ?
+BOOL OR(BOOL A, BOOL B) { return (A || B); }
+BOOL NOR(BOOL A, BOOL B) { return (!A && !B); }// return !OR(A, B); }//
+BOOL AND(BOOL A, BOOL B) { return (A && B); }
+BOOL NAND(BOOL A, BOOL B) { return (!A || !B); }//return !AND(A, B); };//
+
+
+// Caixas
+//-------
+
+Caixas &Caixas::caixa(COORD pos, short width, short height, char ch, WORD attr)
+{
+    // validar a caixa
+    if (NOR(pos.X < 0, pos.Y < 0) && // não tem coordenadas negativas
+        NOR(pos.X > _max.X, pos.Y > _max.Y)) { // não tem coordenadas maiores que _max
+        _caixas.push_back(Caixa(pos, width, height, ch, attr));
+    }
+
+    return *this;
+}
+
+// devolve (iterador da) caixa na posição pos (do mapa)
+vector<Caixa>::iterator Caixas::caixa(COORD pos)
+{
+    vector<Caixa>::iterator it = _caixas.begin();
+    for (it; it != _caixas.end(); it++) {
+        if (pos.X == it->pos().X && pos.Y == it->pos().Y) {
+            return it;
+        }
+    }
+
+    return it;
+}
+
+// devolve caixas definidas numa determinada zona
+vector<Caixa> Caixas::caixas(COORD pos, short width, short height)
+{
+    vector<Caixa> caixas;
+    for (Caixa caixa : _caixas) {
+        // coordenada da caixa
+        COORD P = caixa.pos();
+        // dentro da zona
+        if (NOR(P.X - pos.X < 0, P.X - pos.X > width) &&
+            NOR(P.Y - pos.Y < 0, P.Y - pos.Y > height)) {
+            caixas.push_back(caixa);
+        }
+    }
+    return caixas;
+}
---
+++ caixa.h
@@ -0,0 +1,67 @@
+#ifndef CAIXA_H
+#define CAIXA_H
+
+#include <Windows.h>
+#include <vector>
+
+using namespace std;
+
+class Caixa
+{
+    short _width;
+    short _height;
+    COORD _pos;
+    CHAR_INFO _ci;
+
+    //CHAR_INFO *_buffer;
+public:
+    //CHAR_INFO *buffer() { return _buffer; }
+
+    CHAR_INFO ci() { return _ci; }
+    COORD pos() { return _pos; }
+    COORD size() { return COORD{_width, _height}; }
+    short width() { return _width; }
+    short height() { return _height; }
+
+    void width(short width) { _width = width; }
+    void height(short height) { _height = height; }
+
+    Caixa(COORD pos, short width, short height, char ch = '#', WORD attr = 8) {
+        _width = width;
+        _height = height;
+        _pos = pos;
+
+        _ci.Char.UnicodeChar = ch;
+        _ci.Attributes = attr;
+
+        //_buffer = new CAR_INFO[width*height];
+    }
+    ~Caixa(){
+        //delete[] _buffer;
+    }
+};
+
+// Caixas
+//-------
+class Caixas
+{
+private:
+    //TODO ...
+    COORD _max;// = { 120, 60 };
+    vector<Caixa> _caixas;
+
+public:
+    // adiciona uma caixa
+    Caixas &caixa(COORD pos, short width, short height, char ch = '#', WORD attr = 8);
+
+    // devolve caixa na posição pos
+    vector<Caixa>::iterator caixa(COORD pos);
+
+    // devolve caixas numa determinada zona
+    vector<Caixa> caixas(COORD pos, short width, short height);
+
+    // construtor
+    Caixas(short x = 120, short y = 60) { _max = { x, y }; }
+};
+
+#endif
