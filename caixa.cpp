b86c9501652aaf394424392ce08ce49ea67419d6
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-12-10 00:53:41 +0000

    Update Caixa

--- caixa.cpp
+++ caixa.cpp
@@ -6,7 +6,6 @@ BOOL NOR(BOOL A, BOOL B) { return (!A && !B); }// return !OR(A, B); }//
 BOOL AND(BOOL A, BOOL B) { return (A && B); }
 BOOL NAND(BOOL A, BOOL B) { return (!A || !B); }//return !AND(A, B); };//
 
-
 // Caixas
 //-------
 
@@ -38,15 +37,24 @@ vector<Caixa>::iterator Caixas::caixa(COORD pos)
 vector<Caixa> Caixas::caixas(COORD pos, short width, short height)
 {
     vector<Caixa> caixas;
+
     for (Caixa caixa : _caixas) {
-        // coordenada da caixa
-        COORD P = caixa.pos();
-        // dentro da zona
-        if (NOR(P.X - pos.X < 0, P.X - pos.X > width) &&
-            NOR(P.Y - pos.Y < 0, P.Y - pos.Y > height)) {
-            caixas.push_back(caixa);
+        // cantos da caixa
+        COORD P[4];
+        P[0] = caixa.pos();
+        P[1] = { P[0].X, P[0].Y + caixa.height() - 1 };
+        P[2] = { P[0].X + caixa.width() - 1, P[0].Y };
+        P[3] = { P[0].X + caixa.width() - 1, P[0].Y + caixa.height() - 1 };
+        for (int i = 0; i < 4; i++) {
+            // dentro da zona
+            if (NOR(P[i].X - pos.X < 0, P[i].X - pos.X > width) &&
+                NOR(P[i].Y - pos.Y < 0, P[i].Y - pos.Y > height)) {
+                caixas.push_back(caixa);
+                break;
+            }
         }
     }
+
     return caixas;
 }
 
@@ -59,12 +67,12 @@ vector<Caixa> Caixas::contem(COORD ponto)
         // coordenada da caixa
         COORD P = caixa.pos();
         COORD size = caixa.size();
-
         // dentro da caixa
         if (NOR(ponto.X - P.X < 0, ponto.X - P.X > size.X) &&
             NOR(ponto.Y - P.Y < 0, ponto.Y - P.Y > size.Y)) {
             caixas.push_back(caixa);
         }
     }
+
     return caixas;
 }
