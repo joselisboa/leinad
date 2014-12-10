0f4c977834ac4c0022ed7f701841bd94fd2440ff
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-12-10 13:37:58 +0000

    Update header

--- header.h
+++ header.h
@@ -1,3 +1,10 @@
+//
+// Leinad
+//
+// description: School project for OOP (Object Oriented Programming)
+// author: José Vieira Lisboa
+// url: https://github.com/joselisboa/leinad
+//
 #include <Windows.h>
 #include <iostream>
 #include <vector>
@@ -6,20 +13,20 @@
 // Grelha
 void testeGrelha()
 {
-	Grelha grelha(40, 20);// GRELHA.X, GRELHA.Y);
-	grelha.testes();
-	grelha.offset(COORD{ 21, 3 }).fill('?', Ze().RED_FADE).write(Ze().output_handle());
-	grelha.offset(COORD{ 15, 5 }).fill('#', Ze().MAGENTA_FADE).write(Ze().output_handle());
+    Grelha grelha(40, 20);// GRELHA.X, GRELHA.Y);
+    grelha.testes();
+    grelha.offset(COORD{ 21, 3 }).fill('?', Ze().RED_FADE).write(Ze().output());
+    grelha.offset(COORD{ 15, 5 }).fill('#', Ze().MAGENTA_FADE).write(Ze().output());
 }
 
 // Ze
 void testeZe()
 {
-	cout << "> Ze().textColor(Ze().YELLOW);"<< endl;
-	Ze().textColor(Ze().YELLOW);
-	cout << "> Ze().paintString(\"Jose Daniel\", COORD{ 5, 5 }, Ze().BLUE_FADE | Ze().CYAN_FADE << 4);" << endl;
-	Ze().paintString("Jose Daniel", COORD{ 5, 5 }, Ze().BLUE_FADE | Ze().CYAN_FADE << 4);
-	while (false) {
-	  Ze().input();
-	}
+    cout << "> Ze().textColor(Ze().YELLOW);"<< endl;
+    Ze().textColor(Ze().YELLOW);
+    cout << "> Ze().paintString(\"Jose Daniel\", COORD{ 5, 5 }, Ze().BLUE_FADE | Ze().CYAN_FADE << 4);" << endl;
+    Ze().paintString("Jose Daniel", COORD{ 5, 5 }, Ze().BLUE_FADE | Ze().CYAN_FADE << 4);
+    while (false) {
+        Ze()._input();
+    }
 }
